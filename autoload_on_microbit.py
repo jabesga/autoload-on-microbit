949b1c5d54b298b5063035b163493f85fb688ce6
Jon Ander Besga <jonan.bsg@gmail.com>
2016-09-27 14:11:00 +0200

    little updates

--- autoload_on_microbit.py
+++ autoload_on_microbit.py
@@ -10,21 +10,22 @@ import signal
 import glob
 import shutil
 
-DOWNLOAD_FOLDER = '#SET YOUR DOWNLOAD FOLDER HERE#'
-DESTINATION_DISK = '#SET THE LOCATION OF THE MICROBIT#'
+DOWNLOAD_FOLDER = '/home/jon/Downloads'
+DESTINATION_DISK = '/run/media/jon/MICROBIT'
 
 print("-= Autoload on Micro:Bit =-")
 
 try:
     print("Waiting .hex file...")
     while(1):
-        files_found = glob.glob(DOWNLOAD_FOLDER + '\*.hex')
+        files_found = glob.glob(DOWNLOAD_FOLDER + '/*.hex')
         if len(files_found) > 1:
             print("WARNING: More than one file found. Go to your DOWNLOAD FOLDER and remove .hex files until you have only one.")
             break
         elif len(files_found) == 1:
             print("Detected .hex file. Loading it into Micro:Bit...")
-            shutil.move(files_found[0], DESTINATION_DISK)
+            file_name = files_found[0].split("/")[-1]
+            shutil.move(files_found[0], "{}/{}".format(DESTINATION_DISK, file_name))
             print("SUCCESS: .hex file loaded.")
             print("Waiting .hex file...")
         
